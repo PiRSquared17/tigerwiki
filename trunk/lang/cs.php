1a6b1da7183765fcdab9703105316916011b9a41
andikoerner <andikoerner@95e2ecd0-251a-11de-91f4-d7b858dd2e09>
2009-04-09 15:56:10 +0000

    import the existing lionwiki and fckeditor source

---
+++ trunk/lang/cs.php
@@ -0,0 +1,23 @@
+<?php
+	$T_HOME = "Hlavní strana";
+	$T_HELP = "Nápověda";
+	$T_EDIT = "Editovat";
+	$T_DONE = "Uložit změny";
+	$T_SEARCH = "Hledat";
+	$T_SEARCH_RESULTS	= "Výsledky hledání";
+	$T_LIST_OF_ALL_PAGES =	"Seznam všech stránek";
+	$T_RECENT_CHANGES	= "Poslední změny";
+	$T_LAST_CHANGED =	"Naposledy změněno";
+	$T_HISTORY	= "Historie";
+	$T_NO_HISTORY = "Bez historie.";
+	$T_RESTORE = "Obnovit";
+	$T_PASSWORD = "Heslo";
+	$T_EDIT_SUMMARY = "Shrnutí editace:";
+	$T_ERASE_COOKIE = "Smazat cookies";
+	$T_WIKI_CODE = "Wiki kód";
+	$T_MOVE_TEXT = "Nové jméno:";
+	$T_MOVE = "Přesunout";
+	$T_CREATE_PAGE = "Vytvořit stránku s názvem";
+	$T_PROTECTED_READ = "Pro zobrazení obsahu stránek je nutné zadat heslo: ";
+	$TE_WRONG_PASSWORD = "Zadané heslo není správné.";
+?>
---
+++ trunk/template.php
@@ -0,0 +1,78 @@
+<?php
+
+
+global $subdir, $dirs, $files;
+
+?>
+
+<html>
+	<head>
+		<title><?php print $subdir;?></title>
+		<style type="text/css">
+		    body {
+		        padding: 5px;
+		        font-family: sans-serif;
+		    }
+		    h1 {
+		        font-size: 16pt;
+		    }
+		    .box h2 {
+		        font-size: 15px;
+		        margin: 5px;
+		        border-bottom: solid 1px #600;
+		    }
+		    .box {
+			    float: left;
+			    border: solid 1px #000;		    
+			    height: 80%;
+			    padding: 5px;
+			    background: #ddd;
+		    }
+			.dirs {
+			    width: 35%;
+			    padding: 5px;
+			    margin-right: 5px;
+			}
+			.files {
+			    width: 55%;
+			}
+		</style>
+	</head>
+	<body>
+	    <h1>Select Page to link to</h1>
+	    <div class="box dirs">
+		<h2>Directories</h2>
+		<ul >
+			<?php
+				if($subdir) {
+					$path=explode('/',$subdir);
+					array_pop($path);
+					$parent=join('/',$path);
+					print("<li><a href=\"?dir=$parent\">..</a></li>");
+					$subdir = $subdir . '/';
+				}
+				foreach($dirs as $dir) {
+				    $path='';
+					print("<li><a href=\"?dir=$subdir$dir\">$dir</a></li>");
+				}
+				
+			?>
+		</ul>	
+        </div>
+        <div class="box files">
+		<h2>Files</h2>
+		<ul>
+			<?php
+				foreach($files as $file) {
+					print("<li><a href=\"javascript:".wikilink($subdir.$file)."\">$file</a></li>");
+				}
+			?>
+		</ul></div>
+		<form>New File: <input name="new" type="text"><button onclick=<?php print( '"'. wikilink($subdir."'+document.forms[0]['new'].value+'") .'"'); ?>>Insert</button></form>
+
+		
+	</body>
+</html>
+
+
+
